00f5889cba73737ed5760c1fcca1acdc40b0adb5
Abhishek Jadhav <abhijitj293@gmail.com>
2026-07-04 21:16:57 +0530

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/favicon.ico",
+        destination: "/logo-sm.png",
+        permanent: false,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
